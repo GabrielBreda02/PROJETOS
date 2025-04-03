7c665a5cdb20dac8086fd88f794a311aef73c812
Gabriel Breda Medeiros <164912868+GabrielBreda02@users.noreply.github.com>
2025-04-03 10:47:27 -0300

    estudos back
    estrutura repititiva while

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace EstruturaRepetitivaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um numero :");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("f3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro numero : ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);   
+            }
+            Console.WriteLine("numero negativo ! ");
+        }
+    }
+}
