261030903aba9f6ff2091d2413911612af97d431
Gabriel Breda Medeiros <164912868+GabrielBreda02@users.noreply.github.com>
2025-04-03 10:49:44 -0300

    estudos backend
    exercicios

--- Program.cs
+++ Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercicio6
+namespace ExerciciosDev
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true) {
+            Console.WriteLine("COLOQUE A HORA DE ENTRADA E SAIDA DO JOGO: ");
+            string[] horas = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(horas[0]);
+            int horaFinal = int.Parse(horas[1]);
+            int duracao;
+
+            if (horaInicial < horaFinal ) {
+            duracao = horaFinal - horaInicial;
+            }
+            else { 
+                duracao = 24 - horaInicial + horaFinal;
+                }
+
+            Console.WriteLine(" O JOGO DUROU " + duracao + "HORA(S)");
+
+                Console.WriteLine("deseja encerrar ? DIGITE 'S' PARA SAIR OU QUALQUER OUTRA TECLA PARA CONTINUAR.");
+
+                if(Console.ReadLine(). ToLower()== "s")
+                {
+                break;
+                }
+            }
         }
     }
 }
+
