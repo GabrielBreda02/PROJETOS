269df94197ff8fa7099741dd6d2756506462503f
Gabriel Breda Medeiros <164912868+GabrielBreda02@users.noreply.github.com>
2025-04-03 10:48:55 -0300

    estudos backend
    exercicios

--- Program.cs
+++ Program.cs
@@ -3,43 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Globalization;
 
-namespace Exercicio5
+namespace Exercicio6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" quais itens foram pegos, e a quantidade para o calculo:");
-            string[] tabela = Console.ReadLine().Split(' ');
-            int codigo = int.Parse(tabela[0]);
-            int quantidade = int.Parse(tabela[1]);
-
-            double total;
-
-            if (codigo == 1)
-            {
-                total = quantidade * 4.0;
-            }
-            else if (codigo == 2)
-            {
-                total = quantidade * 4.5;
-            }
-            else if(codigo == 3)
-            {
-                total = quantidade * 5.0;
-            }
-            else if (codigo == 4)
-            {
-                total = quantidade * 2.0;
-            } 
-            else
-            {
-                total = quantidade * 1.5;
-            }
-            Console.WriteLine("total: R$" + total.ToString("f2", CultureInfo.InvariantCulture));
-                
         }
     }
 }
